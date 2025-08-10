59e0c58f349eac61a4050322d5caddcf2e5970f9
rishav_learnerml <rishavbio1@gmail.com>
2025-08-10 17:04:22 +0530

    final api change

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "http://localhost:3000"; // ← change if your API is hosted elsewhere
+// export const API_BASE_URL = "http://localhost:3000"; // ← change if your API is hosted elsewhere
+export const API_BASE_URL = "https://neotutorapi.swagcoder.com"; // ← change if your API is hosted elsewhere
